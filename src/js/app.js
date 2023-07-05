001b4275ccce20bdc964803fb37420fbaaca143c
Anzhalika Turlak <293852647@mail.ru>
2023-07-05 17:04:52 +0300

    fix: add message file js

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,5 @@ document.querySelector('.navigation__burger').addEventListener('click', function
     document.querySelector('.header__top-navigation').classList.toggle('open');
 
 })
+
+console.log('Link form for CROSS-CHECK(CV#3. CV. Cross-Check): https://rolling-scopes-school.github.io/checklist/');
